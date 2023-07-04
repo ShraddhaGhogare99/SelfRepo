c997b245b7b4f470c8fea77655c1f7e5c3d92dca
ShraddhaGhogare99 <shraddhaghogare1899@gmail.com>
2023-07-04 16:42:15 +0530

    First Commit

---
+++ Fruits.java
@@ -0,0 +1,18 @@
+class Fruits{
+
+int a;
+int b;
+
+void sweet(int a,int b){
+int c=a+b;
+System.out.println("Addition"+c);
+}
+
+public static void main(String[] args){
+
+Fruits fav=new Fruits();
+fav.sweet(65,25);
+
+}
+
+}
